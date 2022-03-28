25db6422658e54ff68e9299e16591abba6ae2168
cboittin <cedric.boittin@demaurex.com>
2022-03-28 14:21:37 +0200

    Step2: Clean up the program's directory

--- App.xaml.cs
+++ App.xaml.cs
@@ -15,6 +15,29 @@
     {
         public App()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += this.CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            try
+            {
+                if (args.Name != null && args.Name.StartsWith("SatelliteLocDemo.resources"))
+                {
+                    //string assemblyPath = $"{GetBasePath()}\\{Thread.CurrentThread.CurrentUICulture.Name}\\SatelliteLocDemo.resources.dll";
+                    string assemblyPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\Languages\\{Thread.CurrentThread.CurrentUICulture.Name}\\SatelliteLocDemo.resources.dll";
+                    Assembly assembly = Assembly.LoadFrom(assemblyPath);
+                    return assembly;
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine($"Error loading translations for {args.Name}");
+                Trace.WriteLine(e);
+                return null;
+            }
         }
     }
 }
--- updateDll.bat
+++ updateDll.bat
@@ -1,14 +1,27 @@
-SET resgen="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\ResGen.exe"
-SET al="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\al.exe"
-%resgen% Properties\Resources.resx
-%resgen% Properties\Errors.resx
-%resgen% Properties\Resources.fr-FR.resx
-%resgen% Properties\Errors.fr-FR.resx
-%resgen% Properties\Resources.de-DE.resx
-%resgen% Properties\Errors.de-DE.resx
+set resgen="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\ResGen.exe"
+set al="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\al.exe"
 
-%al% -target:lib -embed:Properties\Resources.resources,SatelliteLocDemo.Properties.Resources.en-US.resources -embed:Properties\Errors.resources,SatelliteLocDemo.Properties.Errors.en-US.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:en-US -out:bin\x64\Debug\en-US\SatelliteLocDemo.resources.dll 
+echo "Compile resx"
+SET resourcesPath="obj\x64\Debug\Properties"
+if not exist %resourcesPath% mkdir %resourcesPath%
+%resgen% Properties\Resources.resx Properties\Resources.resources
+%resgen% Properties\Resources.fr-FR.resx %resourcesPath%\Resources.fr-FR.resources
+%resgen% Properties\Resources.de-DE.resx %resourcesPath%\Resources.de-DE.resources
+%resgen% Properties\Errors.resx Properties\Errors.resources
+%resgen% Properties\Errors.fr-FR.resx %resourcesPath%\Errors.fr-FR.resources
+%resgen% Properties\Errors.de-DE.resx %resourcesPath%\Errors.de-DE.resources
 
-%al% -target:lib -embed:Properties\Resources.fr-FR.resources,SatelliteLocDemo.Properties.Resources.fr-FR.resources -embed:Properties\Errors.fr-FR.resources,SatelliteLocDemo.Properties.Errors.fr-FR.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:fr-FR -out:bin\x64\Debug\fr-FR\SatelliteLocDemo.resources.dll 
+echo "en-US"
+SET enusPath="bin\x64\Debug\Languages\en-US"
+if not exist %enusPath% mkdir %enusPath%
+%al% -embed:Properties\Resources.resources,SatelliteLocDemo.Properties.Resources.en-US.resources -embed:Properties\Errors.resources,SatelliteLocDemo.Properties.Errors.en-US.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:en-US -platform:x64 -out:%enusPath%\SatelliteLocDemo.resources.dll
 
-%al% -target:lib -embed:Properties\Resources.de-DE.resources,SatelliteLocDemo.Properties.Resources.de-DE.resources -embed:Properties\Errors.de-DE.resources,SatelliteLocDemo.Properties.Errors.de-DE.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:de-DE -out:bin\x64\Debug\de-DE\SatelliteLocDemo.resources.dll 
+echo "fr-FR"
+SET frfrPath="bin\x64\Debug\Languages\fr-FR"
+if not exist %frfrPath% mkdir %frfrPath%
+%al% -embed:%resourcesPath%\Resources.fr-FR.resources,SatelliteLocDemo.Properties.Resources.fr-FR.resources -embed:%resourcesPath%\Errors.fr-FR.resources,SatelliteLocDemo.Properties.Errors.fr-FR.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:fr-FR -platform:x64 -out:%frfrPath%\SatelliteLocDemo.resources.dll
+
+echo "de-DE"
+SET dedePath="bin\x64\Debug\Languages\de-DE"
+if not exist %dedePath% mkdir %dedePath%
+%al% -embed:%resourcesPath%\Resources.de-DE.resources,SatelliteLocDemo.Properties.Resources.de-DE.resources -embed:%resourcesPath%\Errors.de-DE.resources,SatelliteLocDemo.Properties.Errors.de-DE.resources -template:bin\x64\Debug\SatelliteLocDemo.dll -culture:de-DE -platform:x64 -out:%dedePath%\SatelliteLocDemo.resources.dll
